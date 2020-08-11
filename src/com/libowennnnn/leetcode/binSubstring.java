1c404fb12a0682652e55392756cece4d19fb1013
Bowen Li <44309584+libowennnnn@users.noreply.github.com>
2020-08-11 15:35:42 +0800

    Update and rename Reverse.java to binSubstring.java
    test

---
+++ src/com/libowennnnn/leetcode/binSubstring.java
@@ -0,0 +1,41 @@
+package com.libowennnnn.leetcode;
+
+public class binSubstring {
+    public int countBinarySubstrings(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(isReverse(s, i)) count++;
+        }
+        return count;
+    }
+
+    public boolean isReverse(String s, int idx){
+        int start = idx + 1;
+        int count = 1;
+        while(start < s.length()){
+            if(s.charAt(start) == s.charAt(idx)) {
+                count++;
+                start++;
+            }
+            else break;
+        }
+        while(start < s.length() && count > 0){
+            if(s.charAt(start) != s.charAt(idx)) {
+                count--;
+                start++;
+            }
+
+            else break;
+        }
+        if (count == 0) return true;
+        return false;
+    }
+}
+
+class TestbinSubstring{
+    public static void main(String[] args){
+        binSubstring t = new binSubstring();
+        int result = t.countBinarySubstrings("00110011");
+        System.out.println(result);
+    }
+}
